a760bd2d7fc4ee30524245b69923e6dfed1f0617
John Joseph <47251026+JohnJoseph2007@users.noreply.github.com>
2022-01-09 03:24:22 +0000

    not too much

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-import Discord from 'discord.js';
+const mySecret = process.env['TOKEN']
+const Discord = require('discord.js')
+
+const client = new Discord.Client()
+
+client.on('ready', ()=>{
+  console.log("The Client Is Ready!")
+})
+
+client.login(mySecret)
